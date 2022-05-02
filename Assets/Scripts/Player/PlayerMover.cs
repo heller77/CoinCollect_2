77f4f86f7e8b5e9f181a245959c570db43ef854b
heller77 <koki001230@icloud.com>
2022-05-02 15:58:04 +0900

    プライヤーの移動クラス作った

---
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityTemplateProjects.Player
+{
+    public class PlayerMover : MonoBehaviour
+    {
+        private Vector3 moveVector = new Vector3();
+        [SerializeField] private float speed = 1.0f;
+
+        private void Update()
+        {
+            moveVector.x = Input.GetAxis("Horizontal");
+            moveVector.z = Input.GetAxis("Vertical");
+        }
+
+        private void FixedUpdate()
+        {
+            Move(moveVector * speed, Time.deltaTime);
+        }
+
+        public void Move(Vector3 diff, float deltatime)
+        {
+            this.transform.position += diff * deltatime;
+        }
+    }
+}
