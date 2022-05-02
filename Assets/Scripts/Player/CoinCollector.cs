1d896b66f13099d23f4b35e3b8ea5313b93fbabf
heller77 <koki001230@icloud.com>
2022-05-02 16:15:09 +0900

    コインに触れたらコインを集めるCoinCollectorクラスを作成

---
+++ Assets/Scripts/Player/CoinCollector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityTemplateProjects.Player
+{
+    public class CoinCollector : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.TryGetComponent(out Items.ITouchable getObject))
+            {
+                getObject.Touch();
+                Debug.Log("取得した！");
+            }
+        }
+    }
+}
