cc8edfe817d3a72ba8cc923941acbdcd582afda7
heller77 <koki001230@icloud.com>
2022-05-03 01:04:37 +0900

    CoinGenerator.csとGameLoop.csを作成

---
+++ Assets/Scripts/Items/Coins/CoinGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Items.Coins
+{
+    public class CoinGenerator : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _coinGameObject;
+
+        [SerializeField] private float _interval=3.0f;
+
+        private List<Coin> _coinList = new List<Coin>();
+        void Generate(CoinType type)
+        {
+            var tmpCoin = Instantiate(_coinGameObject).GetComponent<Coin>();
+            _coinList.Add(tmpCoin);
+            tmpCoin.SetCoinType(type);
+        }
+
+        void GenerateCoins(int goldNum,int silverNum,int copperNum)
+        {
+            for (int i = 0; i < goldNum; i++)
+            {
+                this.Generate(CoinType.Gold);
+            }
+
+            for (int i = 0; i < silverNum; i++)
+            {
+                this.Generate(CoinType.Silver);
+            }
+
+            for (int i = 0; i < copperNum; i++)
+            {
+                this.Generate(CoinType.Copper);
+            }
+        }
+        
+    }
+}
