864a3bd0e81a56988550bddf527222f590a4d916
heller77 <koki001230@icloud.com>
2022-05-02 16:14:30 +0900

    Coinクラスを作成

---
+++ Assets/Scripts/Items/Coin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UnityTemplateProjects.Items
+{
+    public class Coin : MonoBehaviour,ITouchable
+    {
+        private CoinType _type;
+        public void Touch()
+        {
+            Debug.Log("コイン取得した");
+        }
+    }
+}
