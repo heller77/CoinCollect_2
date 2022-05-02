484b223a43f44d27b66c81187c2e4403f8872dca
heller77 <koki001230@icloud.com>
2022-05-02 16:29:57 +0900

    コインのtypeのgetterとsetterを実装

--- Assets/Scripts/Items/Coin.cs
+++ Assets/Scripts/Items/Coin.cs
@@ -1,13 +1,25 @@
 ﻿using UnityEngine;
+using UnityTemplateProjects.Player;
 
 namespace UnityTemplateProjects.Items
 {
     public class Coin : MonoBehaviour,ITouchable
     {
+        [SerializeField]
         private CoinType _type;
         public void Touch()
         {
             Debug.Log("コイン取得した");
+        }
+
+        public void SetCoinType(CoinType type)
+        {
+            this._type = type;
+        }
+
+        public CoinType GetCoinType(CoinType type)
+        {
+            return type;
         }
     }
 }
