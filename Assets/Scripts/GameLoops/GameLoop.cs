f5dbbf75657f75aebb1201731def2e09466f75e3
heller77 <koki001230@icloud.com>
2022-05-03 13:32:46 +0900

    コインをゲーム時間中に、span毎にランダム位置に生成するシステムを実装

--- Assets/Scripts/GameLoops/GameLoop.cs
+++ Assets/Scripts/GameLoops/GameLoop.cs
@@ -1,9 +1,46 @@
-﻿using UnityEngine;
+﻿using System;
+using CoinCollect2.Items.Coins;
+using UnityEngine;
 
 namespace CoinCollect2.GameLoops
 {
     public class GameLoop : MonoBehaviour
     {
+        [SerializeField] private CoinGenerator _coinGenerator;
         
+        //ゲーム時間（デフォルト60s）
+        [SerializeField]
+        private float gameTime=60.0f;
+        [SerializeField]
+        private bool isPlayingNow = false;
+
+        private float spentTime = 0.0f;
+
+        public void setIsPlayingNow(bool isPlayingNow)
+        {
+            this.isPlayingNow = isPlayingNow;
+        }
+        private void Update()
+        {
+            
+
+            //ゲーム中なら
+            if (isPlayingNow)
+            {
+                spentTime += Time.deltaTime;
+                if ( spentTime > gameTime)
+                {
+                    isPlayingNow = false;
+                    Debug.Log("ゲーム終了！");
+                }
+                CallCoinGenerate();
+                
+            }
+        }
+
+        private void CallCoinGenerate()
+        {
+            _coinGenerator.Call(Time.deltaTime);
+        }
     }
 }
--- Assets/Scripts/Items/Coins/CoinGenerator.cs
+++ Assets/Scripts/Items/Coins/CoinGenerator.cs
@@ -8,17 +8,22 @@ namespace CoinCollect2.Items.Coins
         [SerializeField]
         private GameObject _coinGameObject;
 
-        [SerializeField] private float _interval=3.0f;
-
+        // [SerializeField] private float _interval=3.0f;
+        [SerializeField]
         private List<Coin> _coinList = new List<Coin>();
+        [SerializeField] private Vector2 leftDownPoint = new Vector2();
+        [SerializeField] private Vector2 rightUpPoint = new Vector2();
+        [SerializeField] private float y = 1.0f;
         void Generate(CoinType type)
         {
-            var tmpCoin = Instantiate(_coinGameObject).GetComponent<Coin>();
+            var tmpCoinGameObject = Instantiate(_coinGameObject);
+            tmpCoinGameObject.transform.position = new Vector3(Random.Range(leftDownPoint.x,rightUpPoint.x), y, Random.Range(leftDownPoint.y,rightUpPoint.y));
+            var tmpCoin = tmpCoinGameObject.GetComponent<Coin>();
             _coinList.Add(tmpCoin);
             tmpCoin.SetCoinType(type);
         }
 
-        void GenerateCoins(int goldNum,int silverNum,int copperNum)
+        public void GenerateCoins(int goldNum,int silverNum,int copperNum)
         {
             for (int i = 0; i < goldNum; i++)
             {
@@ -35,6 +40,29 @@ namespace CoinCollect2.Items.Coins
                 this.Generate(CoinType.Copper);
             }
         }
-        
+
+        private bool completedFirstGenerate = false;
+        //コイン生成のスパン（間隔）
+        [SerializeField]private float span = 10.0f;
+        //ゲーム開始からの経過時間
+        private float timeToNextGenerate = 0.0f;
+        /// <summary>
+        /// ゲームループ側から呼ばれる
+        /// </summary>
+        /// <param name="deltatime"></param>
+        public void Call(float deltatime)
+        {
+            timeToNextGenerate += deltatime;
+            if (!completedFirstGenerate)
+            {
+                GenerateCoins(10, 10, 10);
+                completedFirstGenerate = true;
+            }
+            if (timeToNextGenerate >= span)
+            {
+                timeToNextGenerate -= span;
+                GenerateCoins(10,10,10);
+            }
+        }
     }
 }
