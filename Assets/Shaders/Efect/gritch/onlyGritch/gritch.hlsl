86f009b11239eb71e7a94aa4407a4bc4869268bd
heller77 <koki001230@icloud.com>
2022-05-12 22:09:31 +0900

    gritchシェーダを追加した

---
+++ Assets/Shaders/Efect/gritch/onlyGritch/gritch.hlsl
@@ -0,0 +1,26 @@
+﻿void uvFilteralpha0_float(in float2 uv,in float4 inCol,out float4 color)
+{
+    color=inCol;
+    if(uv.x<0||uv.x>1||uv.y<0||uv.y>1)
+    {
+        color=float4(0,0,0,0);
+    }
+    
+    
+}
+void uvFilteralpha1_float(in float2 uv,in float4 inCol,out float4 color)
+{
+    color=inCol;
+    if(uv.x<0||uv.x>1||uv.y<0||uv.y>1)
+    {
+        color=float4(0,0,0,1);
+    }
+    
+    
+}
+void noise_float(in float2 uv,in float size,out float color)
+{
+   color=frac(sin(dot(uv*size, float2(12.9898,78.233))) * 43758.5453);;
+    
+    
+}
